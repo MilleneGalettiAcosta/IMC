583f410481528b1d614bb06b6e3db87d3ba365d4
Millene Galetti <mikagaletti1@gmail.com>
2022-10-10 19:23:15 -0300

    Feat: Catch elements html and testing button

---
+++ src/imc.js
@@ -0,0 +1,11 @@
+const name = document.getElementById("name");
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
+const btnCalc = document.getElementById("btn-calc");
+const result = document.getElementById("result");
+
+const imc = () => {
+  alert("Testing button")
+}
+
+btnCalc.addEventListener("click",imc);
