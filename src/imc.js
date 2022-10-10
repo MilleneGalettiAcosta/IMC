6abfd2e7d340340dcba6ea13fd3079ac68802ac1
Millene Galetti <mikagaletti1@gmail.com>
2022-10-10 20:11:08 -0300

    Feat: Add classification

--- src/imc.js
+++ src/imc.js
@@ -1,7 +1,6 @@
 
 const btnCalc = document.getElementById("btn-calc");
 
-
 const imc = () => {
   const nameInput = document.getElementById("name").value;
   const height = document.getElementById("height").value;
@@ -12,7 +11,21 @@ const imc = () => {
 
     const calcImc = (weight / (height * height)).toFixed(1);
 
-    result.textContent = calcImc;
+    let classification = "";
+
+    if(calcImc < 18.5) {
+      classification = "Baixo Peso"
+    } else if(calcImc < 24.9) {
+      classification = "Peso Normal"
+    } else if(calcImc < 29.9) {
+      classification = "Excesso de Peso"
+    } else if (calcImc < 35) {
+      classification = "Obesidade"
+    } else {
+      classification = "Obesidade Extrema"
+    }
+
+    result.textContent = `Olá, ${nameInput}. O seu IMC é de ${calcImc} e de acordo com a tabela de IMC, você está com ${classification}!`;
 
   } else {
     result.textContent = "Preencha todos os campos!!"
