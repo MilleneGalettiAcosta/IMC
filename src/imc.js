7ee82ee3c481a85d6f4364d3b0ecc85d3eabb9f4
Millene Galetti <mikagaletti1@gmail.com>
2022-10-10 19:39:52 -0300

    Fix: Add property value and put in function

--- src/imc.js
+++ src/imc.js
@@ -1,14 +1,17 @@
-const nameInput = document.getElementById("name");
-const height = document.getElementById("height");
-const weight = document.getElementById("weight");
+
 const btnCalc = document.getElementById("btn-calc");
-const result = document.getElementById("result");
+
 
 const imc = () => {
+  const nameInput = document.getElementById("name").value;
+  const height = document.getElementById("height").value;
+  const weight = document.getElementById("weight").value;
+  const result = document.getElementById("result");
+
   if(nameInput !== '' && height !== '' && weight !== '') {
     alert("Everything's OK!")
   } else {
-    alert("Pleade fill in all fields!")
+    result.TextContent = "Preencha todos os campos!!"
   }
 
 }
